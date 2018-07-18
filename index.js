a7d6c3be00b5e84dd7ee1e1d41774f72197628b4
Joy Huang <joyhuangg@gmail.com>
2018-07-18 03:44:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, name){
+  for (let i = 0; i < katzDeliLine.length; i++){
+    if (katzDeliLine[i] === name){
+      return i + 1
+    }
+  }
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0)
+    return "The line is currently empty."
+  let serving = katzDeliLine(shift)
+  return serving
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0)
+    return "The line is currently empty."
+  string = "The line is currently: ";
+  for (int i = 0; i < katzDeliLine.length; i++){
+    string += i + 1
+    string += ". " + katzDeliLine[i] + ", "
+  }
+  string += katzDeliLine.length
+  string += ". " + katzDeliLine[katzDeliLine.length-1]
+  return string
+}
