75b91fd5fb6681caaef6635aeff7713197b38f7b
Joy Huang <joyhuangg@gmail.com>
2018-07-18 03:47:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0)
     return "The line is currently empty."
   let string = "The line is currently: ";
-  for (let i = 0; i < katzDeliLine.length; i++){
+  for (let i = 0; i < katzDeliLine.length-1; i++){
     string += i + 1
     string += ". " + katzDeliLine[i] + ", "
   }
